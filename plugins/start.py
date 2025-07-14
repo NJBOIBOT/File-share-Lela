35ea692cf0d08d94366145411bf554ad94549036
NJBOIBOT <144768572+NJBOIBOT@users.noreply.github.com>
2025-07-14 20:20:22 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -105,16 +105,16 @@ async def start_command(client: Client, message: Message):
         if SHORTLINK_URL or SHORTLINK_API:
             if verify_status['is_verified'] and VERIFY_EXPIRE < (time.time() - verify_status['verified_time']):
                 await db.update_verify_status(user_id, is_verified=False)
-            if &quot;verify_&quot; in message.text and not verify_status['is_verified']:
+            if "verify_" in message.text and not verify_status['is_verified']:
                 try:
-                    _, token = message.text.split(&quot;_&quot;, 1)
+                    _, token = message.text.split("";, 1)
                     if verify_status['verify_token'] != token:
-                        return await message.reply(&quot;Your token is invalid or expired. Try again by clicking /start.&quot;)
+                        return await message.reply("Your token is invalid or expired. Try again by clicking /start.")
                     await db.update_verify_status(id, is_verified=True, verified_time=time.time())
                     current = await db.get_verify_count(id)
                     await db.set_verify_count(id, current + 1)
                 except Exception:
-                    return await message.reply(&quot;Invalid token format. Please click /start and try again.&quot;)
+                    return await message.reply("Invalid token format. Please click /start and try again.")
 
             if not verify_status['is_verified'] and not is_premium:
                 token = ''.join(random.choices(rohit.ascii_letters + rohit.digits, k=10))
